46306562cc19537630b0b4a8b583894ac5926b19
Hasnain Haider <hhaider@adobe.com>
2023-06-11 17:19:32 -0700

    Add docker

--- src/Grid.js
+++ src/Grid.js
@@ -1,23 +1,58 @@
 import './Grid.css';
 import Node from './Node';
+import { useEffect, useState } from 'react';
 
 function Grid() {
-    const row = 5;
-    const col = 5;
-    const cols = [
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 2, 3]
-      ];
+    const [grid, setGrid] = useState([]);
+    // const { height, width, grid } = ApiCall;
+    // for (const x of Array(height).keys()) {
+    //     gridArray.push([]);
+    // }
 
-    const renderGrid = () => cols.map((row, rowIndex) => (
+    // let i = 0;
+    // for (const row of grid) {
+    //   let array = gridArray[i];
+    //   for (const node of row) {
+    //     array.push(node.bounds);
+    //   }
+    //   i++;
+    // }
+
+
+    const mapBoundariesToClassNames = cell => {
+      let classNames = "";
+      for (const boundary of cell.bounds) {
+        if (classNames !== "") {
+          classNames = `${classNames} `;
+        }
+        if (boundary === 'B') {
+          classNames = `${classNames}bottom`;
+        } else if (boundary === 'R') {
+          classNames = `${classNames}right`;
+        }
+      }
+      return classNames;
+    }
+
+    useEffect(() => {
+      // fetch("http://localhost:8080/generatemaze")
+      fetch("http://graph-service:8080/generatemaze")
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response;
+      })
+      .then(res => {
+        console.log(res);
+        setGrid(res.grid);
+      });
+    }, []);
+    
+    const renderGrid = () => grid.map((row, rowIndex) => (
         <div className="row" key={rowIndex}>
         {
-            row.map((cell, colIndex) => <Node col={colIndex} row={rowIndex} key={`${colIndex},${rowIndex}`}/>)
+            row.map((cell, colIndex) => <Node className={mapBoundariesToClassNames(cell)} col={colIndex} row={rowIndex} key={`${colIndex},${rowIndex}`}/>)
         }
         </div>
     ));
@@ -29,5 +64,14 @@ function Grid() {
   );
 }
 
+function get(node) {
+  /*
+          "id": "0,0",
+          "boundaries": [],
+          "row": 0,
+          "col": 0
+  */
+
+}
 
 export default Grid;
--- src/Node.js
+++ src/Node.js
@@ -1,12 +1,19 @@
 import './Node.css';
 
 function Node(props) {
+  let className = '';
+  if (props.className) {
+    className = `${props.className} node`;
+  } else {
+    className = "node";
+  }
   return (
     <div 
-    className="node"
-     row={props.row} 
-     col={props.col} 
-     id={`${props.row},${props.col}`}/>
+      className={className}
+      row={props.row} 
+      col={props.col} 
+      id={`${props.row},${props.col}`}
+     />
   );
 }
 
